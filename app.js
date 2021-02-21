d33e808cf8231c27406984612bfee5b877c4ea9b
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2021-02-21 19:42:15 +0200

    refactor ui, add metadat area

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
-
+var gparams = null;
+var network = null;
 MAX_RESULTS = 30;
+source = default_source;
 
-async function getRelatedAddresses(address) {
+
+async function getRelatedTxs(address) {
     res = await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${token}`)
     rv = await res.json();
     return rv;
 }
 
-async function populate(source) {
+async function getBalance(address) {
+  res = await fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${address}&sort=asc&apikey=${token}`)
+  rv = await res.json();
+  return rv;
+}
 
-    rv = await getRelatedAddresses(source);
+async function populate(source, tx=null) {
+
+    rv = await getRelatedTxs(source);
+    txs = rv.result;
     eff_rv = rv.result.slice(0, MAX_RESULTS);
 
-    // populate the textarea
-    $("#context_data").val(JSON.stringify(rv.result, null, 4))
+    
+    balance = await getBalance(source);
+
+    $("#info_table").empty();
+    if (tx == null){
+      $("#address_display").show()
+      $("#tx_display").hide()
+
+      $("#info_table").append("<tr><td>Address</td><td>" + source + "</td></tr>");
+      $("#info_table").append("<tr><td>Balance</td><td>" + parseInt(balance.result)/1000000000000000000 + "</td></tr>");  
+      $("#context_data").val(JSON.stringify(txs, null, 4))
+      for (var i = 0; i < eff_rv.length; i++){
+            $("#txs_table").append(`<tr><td>${eff_rv[i]['hash']}</td></tr>`);  
+      }
+    } else {
+      $("#address_display").hide()
+      $("#tx_display").show()
+
+
+      for (var i = 0; i < eff_rv.length; i++){
+        tx_data = eff_rv[i];
+        if (tx_data['hash'] == tx){
+          for (var key in tx_data){
+            $("#tx_table").append(`<tr><td>${key}</td><td>${tx_data[key]}</td></tr>`);  
+          }
+          break;
+        }
+      }
+    }
 
     nodesSet = new Set();   
     eff_rv.forEach((item, index)=>{
@@ -39,7 +76,7 @@ async function populate(source) {
 
     edgesData = []
     eff_rv.forEach((item, index)=>{
-      edgesData.push({ from:item.from, to: item.to, arrowhead:'normal', title: item.hash});
+      edgesData.push({ id:item.hash, from:item.from, to: item.to, arrowhead:'normal', title: item.hash});
     })
 
     // create an array with edges
@@ -65,25 +102,29 @@ async function populate(source) {
       edges: {
         length: 300,
         arrows: 'middle' 
-        // arrows: 'to',
-        // arrowStrikethrough: false,
       },
       layout: {
         randomSeed: undefined,
         improvedLayout:true,
         clusterThreshold: 150,
-        // hierarchical: {
-        //   direction: 'UD',
-        // }
       }
     };
-    var network = new vis.Network(container, data, options);
+    network = new vis.Network(container, data, options);
+    
+    network.once('afterDrawing', () => {
+      container.style.height = '100vh'
+    })
 
     // to create click events
     network.on("click", function (params) {
       console.log(params);
+      gparams = params;
       if (params.nodes.length > 0){
+        source = params.nodes[0];
         populate(params.nodes[0]);
+      } 
+      else if (params.edges.length > 0){
+        populate(source,params.edges[0]);
       }
     });
 
